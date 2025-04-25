be60796e23e0aebc1cbef5c785a050f05491fca7
BlindMuadDib <jccrandall14@gmail.com>
2025-04-25 15:38:17 -0400

    list by name

--- service/routes.py
+++ service/routes.py
@@ -96,11 +96,21 @@ def create_products():
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
+
 @app.route("/products", methods=["GET"])
 def list_products():
     """Returns a list of Products"""
     app.logger.info("Request to list Products...")
-    products = Product.all()
+
+    products = []
+    name = request.args.get("name")
+
+    if name:
+        app.logger.info("Find by name: %s, name")
+        products = Product.find_by_name(name)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
 
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
